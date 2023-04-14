404fd639969bc980ba6c3e462f0698ff4cfd3919
ultramagnus8167 <130133338+ultramagnus8167@users.noreply.github.com>
2023-04-14 12:41:41 +0000

    Index Updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,6 @@ import { fetchEventSource } from '@microsoft/fetch-event-source';
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import LoadingDots from '@/components/ui/LoadingDots';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from '@/components/ui/accordion';
 export default function Home() {
     const [query, setQuery] = useState('');
     const [loading, setLoading] = useState(false);
@@ -14,7 +13,7 @@ export default function Home() {
     const [messageState, setMessageState] = useState({
         messages: [
             {
-                message: 'Hi, what would you like to learn about this legal case?',
+                message: 'Hi, I\'m Optimus Prime ',
                 type: 'apiMessage',
             },
         ],
@@ -137,7 +136,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+           Custom AI Assistant
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -166,43 +165,8 @@ export default function Home() {
                           </ReactMarkdown>
                         </div>
                       </div>
-                      {message.sourceDocs && (<div className="p-5" key={`sourceDocsAccordion-${index}`}>
-                          <Accordion type="single" collapsible className="flex-col">
-                            {message.sourceDocs.map((doc, index) => (<div key={`messageSourceDocs-${index}`}>
-                                <AccordionItem value={`item-${index}`}>
-                                  <AccordionTrigger>
-                                    <h3>Source {index + 1}</h3>
-                                  </AccordionTrigger>
-                                  <AccordionContent>
-                                    <ReactMarkdown linkTarget="_blank">
-                                      {doc.pageContent}
-                                    </ReactMarkdown>
-                                    <p className="mt-2">
-                                      <b>Source:</b> {doc.metadata.source}
-                                    </p>
-                                  </AccordionContent>
-                                </AccordionItem>
-                              </div>))}
-                          </Accordion>
-                        </div>)}
                     </>);
         })}
-                {sourceDocs.length > 0 && (<div className="p-5">
-                    <Accordion type="single" collapsible className="flex-col">
-                      {sourceDocs.map((doc, index) => (<div key={`SourceDocs-${index}`}>
-                          <AccordionItem value={`item-${index}`}>
-                            <AccordionTrigger>
-                              <h3>Source {index + 1}</h3>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                              <ReactMarkdown linkTarget="_blank">
-                                {doc.pageContent}
-                              </ReactMarkdown>
-                            </AccordionContent>
-                          </AccordionItem>
-                        </div>))}
-                    </Accordion>
-                  </div>)}
               </div>
             </div>
             <div className={styles.center}>
@@ -210,7 +174,7 @@ export default function Home() {
                 <form onSubmit={handleSubmit}>
                   <textarea disabled={loading} onKeyDown={handleEnter} ref={textAreaRef} autoFocus={false} rows={1} maxLength={512} id="userInput" name="userInput" placeholder={loading
             ? 'Waiting for response...'
-            : 'What is this legal case about?'} value={query} onChange={(e) => setQuery(e.target.value)} className={styles.textarea}/>
+            : 'How can I help you 🌞?'} value={query} onChange={(e) => setQuery(e.target.value)} className={styles.textarea}/>
                   <button type="submit" disabled={loading} className={styles.generatebutton}>
                     {loading ? (<div className={styles.loadingwheel}>
                         <LoadingDots color="#000"/>
@@ -228,11 +192,6 @@ export default function Home() {
               </div>)}
           </main>
         </div>
-        <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
-          </a>
-        </footer>
       </Layout>
     </>);
 }
