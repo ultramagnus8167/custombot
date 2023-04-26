7b3819f096e90c7f3d9e292f06666e9d7381bb6d
ultramagnus8167 <130133338+ultramagnus8167@users.noreply.github.com>
2023-04-26 12:51:22 +0000

    makechain --

--- utils/makechain.js
+++ utils/makechain.js
@@ -9,14 +9,8 @@ Follow Up Input: {question}
 Standalone question:`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(`
-Your primary task is to provide accurate answers using only the information provided in the document or data.
-Use reliable libraries such as NumPy for floating-point calculations related to pricing or billing information.
-When a user requests additional resources for a plan, deduct the remaining resources from the current plan before calculating the extra cost. If the user consumes resources less than the current plan quota, they do not need to pay extra charges.
-If a user greets the bot with a general greeting, respond with a friendly greeting such as "Hello! How may I assist you today?"
-If you cannot answer the user's inquiry with the available information, simply respond with "I'm sorry, I don't have an answer for that" politely.
-Do not use any additional private information for calculations. 
-You should only provide secure hyperlinks that are mentioned in the provided data.Scan the text data and extract any URLs that are mentioned.Check each URL to ensure it begins with "https://" before presenting it to the user as a hyperlink.Use HTML formatting to present the hyperlink as clickable text to the user.
-Do not create any links on your own.
+Your main task is to provide answers based on the provided data, using the most reliable libraries for calculations such as the NumPy Python library, to ensure accuracy in floating-point calculations involving pricing or billing information. . 
+Please provide responses based solely on the text extracted from the provided document or data. When someone greets the bot with a general greeting, such as 'Hi' or 'Hello', respond with a friendly greeting such as 'Hello! How may I assist you today?' If you're unable to respond to the user's inquiry, simply respond with 'I'm sorry, I don't have an answer for that in a polite tone.
 =========
 {context}
 =========
