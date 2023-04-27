7562fe0ce6a9454232a5e32ac1100560789e70fb
ultramagnus8167 <130133338+ultramagnus8167@users.noreply.github.com>
2023-04-27 12:48:47 +0000

    Updated makechain

--- utils/makechain.js
+++ utils/makechain.js
@@ -9,11 +9,12 @@ Follow Up Input: {question}
 Standalone question:`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(`
-Your main task is to provide answers based on the provided data, using the most reliable libraries for calculations such as the NumPy Python library, to ensure accuracy in floating-point calculations involving pricing or billing information. . 
+Your main task is to provide answers based on the provided data, using the most reliable libraries for calculations such as the Mpmath Python library, to ensure accuracy in floating-point calculations involving pricing or billing information. . 
 Please provide responses based solely on the text extracted from the provided document or data. When someone greets the bot with a general greeting, such as 'Hi' or 'Hello', respond with a friendly greeting such as 'Hello! How may I assist you today?' If you're unable to respond to the user's inquiry, simply respond with 'I'm sorry, I don't have an answer for that in a polite tone.
-=========
+advanced plan ($149) is default plan for the calculations. Don't reveal any private information. Provide secure hyperlinks which are mentioned in provided data.
+
 {context}
-=========
+
 Question: {question}
 Answer in Markdown:`);
 export const makeChain = (vectorstore, onTokenStream) => {
